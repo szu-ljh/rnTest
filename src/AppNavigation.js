9090d700838231972d47165707d5319ba660c473
szu-ljh <2019281077@email.szu.edu.cn>
2021-07-27 11:23:13 +0800

    navigation

---
+++ src/AppNavigation.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Platform, Image} from 'react-native';
+import HomeContainer from './containers/tabar/HomeContainer';
+import MeContainer from './containers/tabar/MeContainer';
+import ToConnectContainer from './containers/tabar/ToConnectContainer';
+import LoginContainer from './containers/login/LoginContainer';
+import RegisterContainer from './containers/login/RegisterContainer';
+import ECGMoniterContainer from './containers/mesure/ECGMoniterContainer';
+import NbpMoniterContainer from './containers/mesure/NbpMoniterContainer';
+import RespMoniterContainer from './containers/mesure/RespMoniterContainer';
+import Spo2MoniterContainer from './containers/mesure/Spo2MoniterContainer';
+import TempMoniterContainer from './containers/mesure/TempMoniterContainer';
+import ForgetContainer from './containers/login/ForgetContainer';
+import {useTheme} from 'react-native-paper';
+
+if (Platform.OS === 'ios') {
+  MaterialCommunityIcons.loadFont();
+}
+const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function HomeTabs() {
+  const {colors} = useTheme();
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeContainer"
+      //labeled={true}
+      //shifting={true}
+      activeColor="#3A85F8"
+      inactiveColor="#B4B4B4"
+      barStyle={{backgroundColor: '#FFFFFF', height:55}}>
+      <Tab.Screen
+        name="home"
+        component={HomeContainer}
+        options={{
+          tabBarLabel: '首页',
+          tabBarIcon: ({focused, color}) => (
+            <Image
+              source={
+                focused
+                  ? require('./img/tabmain1.png')
+                  : require('./img/tabmain.png')
+              }
+              style={{
+                width: 28,
+                height: 28,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="history"
+        component={ToConnectContainer}
+        options={{
+          tabBarLabel: '设备',
+          tabBarIcon: ({focused, color}) => (
+            <Image
+              source={
+                focused
+                  ? require('./img/tabvideo1.png')
+                  : require('./img/tabvideo.png')
+              }
+              style={{
+                width: 28,
+                height: 28,
+                //tintColor: focused ? '#81D8CF' : '#B4B4B4',
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="me"
+        component={MeContainer}
+        options={{
+          tabBarLabel: '我的',
+          tabBarIcon: ({focused, color}) => (
+            <Image
+              source={
+                focused
+                  ? require('./img/tabme1.png')
+                  : require('./img/tabme.png')
+              }
+              style={{
+                width: 28,
+                height: 28,
+              }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function () {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="login" headerMode="none" mode="card">
+        <Stack.Screen name="login" component={LoginContainer} />
+        <Stack.Screen name="home" component={HomeTabs} />
+        <Stack.Screen name="foget" component={ForgetContainer}/>
+        <Stack.Screen name="register" component={RegisterContainer} />
+        <Stack.Screen name="ecg" component={ECGMoniterContainer}/>
+        <Stack.Screen name="nbp" component={NbpMoniterContainer}/>
+        <Stack.Screen name="resp" component={RespMoniterContainer}/>
+        <Stack.Screen name="spo2" component={Spo2MoniterContainer}/>
+        <Stack.Screen name="temp" component={TempMoniterContainer}/>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/containers/bluetooth/BluetoothConnectContainer.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  SafeAreaView, 
+  StyleSheet, 
+  View,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+  ScrollView,
+  Image} from 'react-native';
+import {Text} from 'react-native-paper';
+import {useSelector, useDispatch} from 'react-redux';
+import BluetoothConnectComponent from '../../components/BluetoothConnectComponent';
+import * as ScreenUtil from '../../utils/ScreenUtil';
+
+const{width, height, scale} = Dimensions.get('window');
+
+const BluetoothConnnectContainer = ({navigation}) => {
+  let data = useSelector((state) => state.bluetooth);
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* 标题栏 */}
+      <View style={styles.topbar}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <Image source={require("../../img/backleft.png")}
+            style={styles.backbtn}/>
+        </TouchableOpacity>
+        <Text style={styles.title}>添加设备</Text>
+      </View>
+      <ScrollView >
+      <BluetoothConnectComponent isable ={data.isable}/>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width,
+    height,
+    backgroundColor:"#FFFFFF",
+    flexDirection:"column"
+  },
+  topbar:{
+    width,
+    height:ScreenUtil.scaleHeight(50),
+    backgroundColor:"#2C65F7",
+    flexDirection:"row"
+  },
+  title:{
+    fontSize:ScreenUtil.setSpText(18),
+    color:"#FFFFFF",
+    marginLeft:ScreenUtil.scaleSize(114),
+    marginTop:ScreenUtil.scaleHeight(14)
+  },
+  backbtn:{
+    width:ScreenUtil.scaleSize(13),
+    height:ScreenUtil.scaleHeight(21),
+    marginLeft:ScreenUtil.scaleSize(16),
+    marginTop:ScreenUtil.scaleHeight(14)
+  },
+  fixedFooter: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd'
+  },
+  button: {
+    height: ScreenUtil.scaleHeight(36),
+    margin: 5,
+    paddingHorizontal: ScreenUtil.scaleSize(16),
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#7B1FA2',
+    fontWeight: 'bold',
+    fontSize: ScreenUtil.setSpText(14)
+  },
+  buttonRaised: {
+    backgroundColor: '#7B1FA2',
+    borderRadius: 2,
+    elevation: 2
+  }
+});
+export default BluetoothConnnectContainer;
